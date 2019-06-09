1ef45981024ffd73eb48488bb39423be8cbcfa09
Diego Ortega <47539238+DiegoGitOrtega@users.noreply.github.com>
2019-06-09 21:51:44 +0200

    Add files via upload

---
+++ doggies.js
@@ -0,0 +1,46 @@
+var doggies=["img/1.jpg","img/2.jpeg","img/3.jpeg","img/4.jpg","img/5.jpg","img/6.jpg"];
+var perrito=0;
+
+function switchDoggie(){
+dogimg.src=doggies[perrito];
+perrito++;
+if(perrito>=doggies.length){
+perrito=0;
+}
+}
+
+setInterval(switchDoggie , 2000);
+
+function gio() {
+flecha.addEventListener("click",switchDoggie);
+}
+
+window.addEventListener("load",gio);
+
+function doggoBackwards(){
+dogimg.src=doggies[perrito];
+perrito--;
+if (perrito<=0) {perrito=5;}
+}
+
+
+function pickDoggo() {
+for (i=0;i<formulario.botonera.length;i++) {
+if (formulario.botonera[i].checked){
+break;}
+}
+}
+
+function giovy() {
+for (i=0;i<formulario.botonera.length;i++) {
+formulario.botonera[i].addEventListener("change", switchDoggie);
+}
+}
+
+window.addEventListener("load",giovy);
+			
+			
+			
+			
+			
+			
